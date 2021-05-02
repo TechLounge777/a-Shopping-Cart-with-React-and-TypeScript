1a355923152a827f00e9cf9c5bcd18ef5de5ef50
TechLounge777 <msblanche777@gmail.com>
2021-05-02 15:16:50 +0200

    added Item.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App = () => {
 
   const getTotalItems = () => null;
 
-  const handleAddToCart = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
 
   const handleRemoveFromCart = () => null;
 
